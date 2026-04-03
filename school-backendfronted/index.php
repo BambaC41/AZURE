e74b7bca4753e0f8f4913d73a136a095fcabe3ee
Bamba Cisse <bamba.cisse@publicisresources.com>
2026-04-03 17:45:01 +0200

    end of the project

--- school-backendfronted/index.php
+++ school-backendfronted/index.php
@@ -644,7 +644,7 @@ $editNote = $selectedEditNote && isset(index_by_id($notes)[$selectedEditNote]) ?
             <div class="card hero-card">
                 <h2>Plateforme de gestion académique</h2>
                 <p>
-                    TEST DU CI CD Supervise les établissements, organise les classes, suit les étudiants, gère les matières et pilote les notes depuis une interface unique.
+                    Supervise les établissements, organise les classes, suit les étudiants, gère les matières et pilote les notes depuis une interface unique.
                     Toutes les statistiques visibles ici sont alimentées par l’API : moyenne générale, rang, mention, statut, moyenne de classe et indicateurs globaux.
                 </p>
             </div>
